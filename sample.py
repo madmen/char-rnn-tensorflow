b3c1558fac82212d25af0d626b9fac1820c19063
roman <roman.abstract@gmail.com>
2017-01-19 00:37:16 +0200

    added aeneid, constitution and tax code to datasets

--- sample.py
+++ sample.py
@@ -16,7 +16,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_dir', type=str, default='save',
                        help='model directory to store checkpointed models')
-    parser.add_argument('-n', type=int, default=500,
+    parser.add_argument('-n', type=int, default=2000,
                        help='number of characters to sample')
     parser.add_argument('--prime', type=text_type, default=u' ',
                        help='prime text')
